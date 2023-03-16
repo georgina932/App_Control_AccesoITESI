fa62060427adc37108eccc043d958f50307e97d2
Georgina <georginaalc4010@gmail.com>
2023-03-16 09:03:37 -0600

    Cambio de color

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.light().copyWith(
         colorScheme: ColorScheme.fromSwatch().copyWith(
           // change the appbar color
-          primary: Color(0xFF0E67B4),
+          primary: Color.fromARGB(0, 154, 11, 11 ),
         ),
       ),
       home: ScanQrPage(),
